886fee9e4f9b11136739cdd12b425744eb8d5cc3
ISAACX4 <isaacnife99@gmail.com>
2025-07-30 15:56:52 +0100

    Add requirements.txt and update .gitignore

--- bizmanager/settings.py
+++ bizmanager/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h2!r=i_vw7vrohg)qcr%^%^6$^952fj(bf0=ihl)&pufbn5x!g'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
